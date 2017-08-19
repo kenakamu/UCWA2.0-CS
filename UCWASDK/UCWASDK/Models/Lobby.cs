e14279cec1661191c324560b79cedea5933c67c8
Rob Taylor <bobt_37@hotmail.com>
2017-08-19 17:34:58 +0100

    Update Lobby object so Participants can be accessed (#25)
    Merged

--- UCWASDK/UCWASDK/Models/Lobby.cs
+++ UCWASDK/UCWASDK/Models/Lobby.cs
@@ -1,23 +1,34 @@
-﻿using Newtonsoft.Json;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Skype.UCWA.Services;
+using Newtonsoft.Json;
 
 namespace Microsoft.Skype.UCWA.Models
 {
     /// <summary>
-    /// Represents a view of the participants who have not yet been admitted to an onlineMeeting. 
+    /// Represents a view of the participants who have not yet been admitted to an onlineMeeting.
     /// A participant in the lobby can be admitted by another participant in the organizer or presenter role. A participant in the lobby who is not admitted will eventually be disconnected. Lobby settings are controlled by the meeting organizer using the accessLevel property of onlineMeeting. 
     /// </summary>
-    public class Lobby : UCWAModelBaseLink
-    {       
-        [JsonProperty("_embedded")]
-        internal InternalEmbedded Embedded { get; set; }
+    public class Lobby : UCWAModelBase
+    {
+        [JsonProperty("_links")]
+        internal InternalLinks Links { get; set; }
 
         [JsonIgnore]
-        public Participant[] Participants { get { return Embedded.participants; } }
+        public string Self { get { return Links.self.Href; } }
 
-        internal class InternalEmbedded
+        internal class InternalLinks
         {
+            [JsonProperty("self")]
+            internal UCWAHref self { get; set; }
+
             [JsonProperty("participant")]
-            internal Participant[] participants { get; set; }
+            internal UCWAHref[] participant { get; set; }
+        }
+
+        public async Task<List<Participant>> GetParticipants()
+        {
+            return await HttpService.GetList<Participant>(Links.participant);
         }
     }
 }

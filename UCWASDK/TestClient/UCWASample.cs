a6b4cfca08464ec233508cc88c7447a2920cf84e
Kenichiro Nakamura <kenakamu@microsoft.com>
2017-10-16 10:43:01 +0900

    Update sample to use retry policy

--- UCWASDK/TestClient/UCWASample.cs
+++ UCWASDK/TestClient/UCWASample.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Skype.UCWA;
 using Microsoft.Skype.UCWA.Enums;
 using Microsoft.Skype.UCWA.Models;
+using Microsoft.Skype.UCWA.RetryPolicies;
 using System;
 using System.Linq;
 using System.Net.Http.Headers;
@@ -14,7 +15,8 @@ namespace TestClient
 
         public UCWASample()
         {
-            client = new UCWAClient();
+            // Instantiate UCWA Client by setting retry policy.
+            client = new UCWAClient(new LinearTransientErrorHandlingPolicy(1000, 10));
             client.SendingRequest += (client, resource) => { client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", TokenService.AquireAADToken(resource)); };
 
             client.ContactPresenceUpdated += Client_ContactPresenceUpdated;
